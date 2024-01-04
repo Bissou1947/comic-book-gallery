7e43c4226817468063bced1fcf1d640f5ea91fce
Bissou1947 <bassil_1947@hotmail.com>
2024-01-04 13:13:07 +0200

    create Details view in ComicBooksController for strongly type sending data

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,27 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        // GET: ComicBooks
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult Detalis()
+        {
+            var comicBook = new ComicBook
+            {
+                seriesTitle = "The Amazing Spider-Man",
+                issueNumber = 700,
+                descriptionHtml = "<p>Final issue! Witness...<strong>will Pete</strong></P>",
+                Artists = new Artist[]
+                {
+                    new Artist{name="Dan Solt",role="Script"},
+                    new Artist{name="Victor oro",role="Pencils"},
+                    new Artist{name="chris brown",role="letters"},
+                    new Artist{name="edgar obsa",role="Colors"}
+                }
+            };
+
+            return View(comicBook);
         }
     }
 }
