79f546ec52a2da01bcfe0b5d966af1580fcec1dc
Bissou1947 <bassil_1947@hotmail.com>
2024-01-04 12:42:54 +0200

    add models

---
+++ src/ComicBookGallery/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int id { get; set; }
+        public int issueNumber { get; set; }
+        public string seriesTitle { get; set; }
+        public string descriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool favorite { get; set; }
+    }
+}
